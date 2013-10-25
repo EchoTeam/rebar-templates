c1f1d5abb23e5cb7b881c4dc4683f40a622c9d7b
snaky <snaky@aboutecho.com>
2013-10-25 15:32:20 +0400

    Fix info in run.me.first.sh regarding 'git push'

--- service_run.me.first.sh
+++ service_run.me.first.sh
@@ -7,6 +7,6 @@ git commit -m "Initial commit"
 echo "############################################################"
 echo "# Run manually:"
 echo "#   git remote add origin git@git.jacknyfe.net:{{name}}.git"
-echo "#   git push"
+echo "#   git push origin master"
 echo "############################################################"
 rm run.me.first.sh
